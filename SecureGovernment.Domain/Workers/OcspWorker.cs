6da73d0251e5a655fe4bdec5424aea4194798cc1
Alexander Gebhard <39505376+a1g3@users.noreply.github.com>
2019-07-26 20:49:12 -0500

    Temporarily remove OCSP worker

--- SecureGovernment.Domain/Workers/OcspWorker.cs
+++ SecureGovernment.Domain/Workers/OcspWorker.cs
@@ -12,38 +12,38 @@ using System.Threading.Tasks;
 
 namespace SecureGovernment.Domain.Workers
 {
-    public class OcspWorker : IAsyncWorker
-    {
-        public async Task<List<ScanResult>> Scan(WorkerInformation workerInformation)
-        {
-            var ocsp = new Ocsp(workerInformation.Issuer, workerInformation.Certificate);
-            byte[] reqArray = ocsp.CreateOcspReq().GetEncoded();
-            var uris = ocsp.GetOcspUris();
-            OcspResp resp;
+    //public class OcspWorker : IAsyncWorker
+    //{
+    //    public async Task<List<ScanResult>> Scan(WorkerInformation workerInformation)
+    //    {
+    //        var ocsp = new Ocsp(workerInformation.Issuer, workerInformation.Certificate);
+    //        byte[] reqArray = ocsp.CreateOcspReq().GetEncoded();
+    //        var uris = ocsp.GetOcspUris();
+    //        OcspResp resp;
 
-            foreach (var uri in uris)
-            {
-                try
-                {
-                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-                    request.Method = "POST";
-                    var requestStream = request.GetRequestStream();
-                    request.ContentLength = reqArray.Length;
-                    request.ContentType = "application/ocsp-request";
-                    request.Accept = "application/ocsp-response";
-                    requestStream.Write(reqArray, 0, reqArray.Length);
+    //        foreach (var uri in uris)
+    //        {
+    //            try
+    //            {
+    //                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+    //                request.Method = "POST";
+    //                var requestStream = request.GetRequestStream();
+    //                request.ContentLength = reqArray.Length;
+    //                request.ContentType = "application/ocsp-request";
+    //                request.Accept = "application/ocsp-response";
+    //                requestStream.Write(reqArray, 0, reqArray.Length);
 
-                    using (WebResponse response = await request.GetResponseAsync())
-                    using (Stream stream = response.GetResponseStream())
-                    {
-                        resp = new OcspResp(stream);
-                    }
-                    return null;
-                    //return (BasicOcspResp)resp.GetResponseObject();
-                }
-                catch { }
-            }
-            return null;
-        }
-    }
+    //                using (WebResponse response = await request.GetResponseAsync())
+    //                using (Stream stream = response.GetResponseStream())
+    //                {
+    //                    resp = new OcspResp(stream);
+    //                }
+    //                return null;
+    //                //return (BasicOcspResp)resp.GetResponseObject();
+    //            }
+    //            catch { }
+    //        }
+    //        return null;
+    //    }
+    //}
 }
