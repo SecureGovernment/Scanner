51407c7450d1ea429503057042bb3e0058371484
Alexander Gebhard <39505376+a1g3@users.noreply.github.com>
2019-10-05 09:44:28 -0500

    Add API project

---
+++ src/SecureGovernment.Api/Controllers/ScanController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SecureGovernment.Domain.Interfaces.Facades;
+using SecureGovernment.Domain.Models.DnsRecords.Results;
+using System.Threading.Tasks;
+
+namespace SecureGovernment.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScanController : ControllerBase
+    {
+        public IScannerFacade ScannerFacade { get; set; }
+
+        [HttpGet]
+        public async Task<JsonResult> Get(string domain)
+        {
+            var workerInformation = new WorkerInformation() { Hostname = domain };
+            var scanResults = await ScannerFacade.ScanDns(workerInformation);
+
+            return new JsonResult(scanResults, new Newtonsoft.Json.JsonSerializerSettings() { NullValueHandling = NullValueHandling.Include });
+        }
+    }
+}
