eae9ffd389ab0b0383b2d7321120656cfc46e8da
Alexander Gebhard <39505376+a1g3@users.noreply.github.com>
2019-10-18 20:12:25 -0500

    Initial commit with webapi

--- src/SecureGovernment/Startup.cs
+++ src/SecureGovernment/Startup.cs
@@ -1,12 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace SecureGovernment
 {
@@ -22,37 +28,129 @@ namespace SecureGovernment
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllers();
+            services.AddSpaStaticFiles(options => options.RootPath = "client-app/dist");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseAuthorization();
+
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "client-app";
+                if (env.IsDevelopment())
                 {
-                    HotModuleReplacement = true
-                });
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+                    app.UseCors(policy => policy
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin());
 
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-
-                routes.MapSpaFallbackRoute(
-                    name: "spa-fallback",
-                    defaults: new { controller = "Home", action = "Index" });
+                    spa.UseVueDevelopmentServer();
+                }
             });
         }
     }
+
+    public static class VueConnection
+    {
+        private static int Port { get; } = 8080;
+        private static Uri DevelopmentServerEndpoint { get; } = new Uri($"http://localhost:{Port}");
+        private static TimeSpan Timeout { get; } = TimeSpan.FromSeconds(60);
+
+        private static string DoneMessage { get; } = "DONE  Compiled successfully in";
+
+        public static void UseVueDevelopmentServer(this ISpaBuilder spa)
+        {
+            spa.UseProxyToSpaDevelopmentServer(async () =>
+            {
+                var loggerFactory = spa.ApplicationBuilder.ApplicationServices.GetService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger("Vue");
+
+                if (IsRunning())
+                {
+                    return DevelopmentServerEndpoint;
+                }
+
+
+                var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+                var processInfo = new ProcessStartInfo
+                {
+                    FileName = isWindows ? "cmd" : "npm",
+                    Arguments = $"{(isWindows ? "/c npm " : "")}run serve",
+                    WorkingDirectory = "client-app",
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                };
+                var process = Process.Start(processInfo);
+                var tcs = new TaskCompletionSource<int>();
+                _ = Task.Run(() =>
+                {
+                    try
+                    {
+                        string line;
+                        while ((line = process.StandardOutput.ReadLine()) != null)
+                        {
+                            logger.LogInformation(line);
+                            if (!tcs.Task.IsCompleted && line.Contains(DoneMessage))
+                            {
+                                tcs.SetResult(1);
+                            }
+                        }
+                    }
+                    catch (EndOfStreamException ex)
+                    {
+                        logger.LogError(ex.ToString());
+                        tcs.SetException(new InvalidOperationException("'npm run serve' failed.", ex));
+                    }
+                });
+                _ = Task.Run(() =>
+                {
+                    try
+                    {
+                        string line;
+                        while ((line = process.StandardError.ReadLine()) != null)
+                        {
+                            logger.LogError(line);
+                        }
+                    }
+                    catch (EndOfStreamException ex)
+                    {
+                        logger.LogError(ex.ToString());
+                        tcs.SetException(new InvalidOperationException("'npm run serve' failed.", ex));
+                    }
+                });
+
+                var timeout = Task.Delay(Timeout);
+                if (await Task.WhenAny(timeout, tcs.Task) == timeout)
+                {
+                    throw new TimeoutException();
+                }
+
+                return DevelopmentServerEndpoint;
+            });
+
+        }
+
+        private static bool IsRunning() => IPGlobalProperties.GetIPGlobalProperties()
+                .GetActiveTcpListeners()
+                .Select(x => x.Port)
+                .Contains(Port);
+    }
+
 }
