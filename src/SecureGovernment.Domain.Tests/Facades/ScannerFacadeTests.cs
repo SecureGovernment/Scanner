67681b93f4242b79c3a3683b9aadbf5318675b45
Alexander Gebhard <39505376+a1g3@users.noreply.github.com>
2019-12-14 21:31:33 -0600

    Fix logger tests

--- src/SecureGovernment.Domain.Tests/Facades/ScannerFacadeTests.cs
+++ src/SecureGovernment.Domain.Tests/Facades/ScannerFacadeTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using SecureGovernment.Domain.Facades;
 using SecureGovernment.Domain.Models;
+using Serilog;
 using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 using static SecureGovernment.Domain.Tests.Mocks.ConnectionMocker;
@@ -36,16 +37,21 @@ namespace SecureGovernment.Domain.Tests.Facades
         {
             // Arrange
             var url = "google.com";
-            var facadeMock = new Mock<ScannerFacade>() { CallBase = true };
+            var facadeMock = Utils.CreateMock<ScannerFacade>();
+            var loggerMock = Utils.CreateMock<ILogger>();
             var cert = new X509Certificate2();
             var chain = new X509Chain();
             facadeMock.Setup(x => x.CreateConnection(url)).Returns(MockConnection(url: url, throwLoadCertificateException: true));
-        
+            loggerMock.Setup(x => x.Error(It.IsAny<SocketException>(), "Cannot connect to google.com.")).Verifiable();
+
             var facade = facadeMock.Object;
+            facade.Logger = loggerMock.Object;
+
             // Act
             var workerInformation = facade.ConnectToTarget(url);
-        
+
             // Assert
+            loggerMock.Verify(x => x.Error(It.IsAny<SocketException>(), "Cannot connect to google.com."), Times.Once);
             Assert.AreEqual("google.com", workerInformation.Hostname);
             Assert.IsNull(workerInformation.Certificate);
             Assert.IsNull(workerInformation.Chain);
