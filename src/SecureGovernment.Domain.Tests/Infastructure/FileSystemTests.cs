bde85c1f1bcc2074493bb94401c37946cd0ee86e
Alexander Gebhard <39505376+a1g3@users.noreply.github.com>
2019-12-21 11:06:48 -0600

    Add FileSystemTests

---
+++ src/SecureGovernment.Domain.Tests/Infastructure/FileSystemTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecureGovernment.Domain.Infastructure;
+using System;
+using System.IO;
+
+namespace SecureGovernment.Domain.Tests.Infastructure
+{
+    [TestClass]
+    public class FileSystemTests
+    {
+        [TestMethod]
+        public void Test_FileSystem_Exists_FileDoesNotExist()
+        {
+            //Arrange
+            var fileSystem = new FileSystem();
+
+            //Act
+            var exists = fileSystem.Exists("DOESNOTEXIST");
+
+            //Assert
+            Assert.IsFalse(exists);
+        }
+
+        [TestMethod]
+        public void Test_FileSystem_Exists_FileExists()
+        {
+            //Arrange
+            var fileSystem = new FileSystem();
+
+            //Act
+            var exists = fileSystem.Exists(Path.Combine(Environment.CurrentDirectory, "SecureGovernment.Domain.dll"));
+
+            //Assert
+            Assert.IsTrue(exists);
+        }
+    }
+}
